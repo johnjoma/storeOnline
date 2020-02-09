b57d2deaf189508457030c2dc812acd32d323d42
johnjoma <irungujohnkimani@gmail.com>
2020-02-09 18:08:58 +0300

    New

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
+import { Product-alertsComponent } from './product-alerts.component';
+
+import { ProductalertsComponent } from './ProductAlerts.component';
+
+import { Product-alertsComponent } from './product-alerts.component';
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
